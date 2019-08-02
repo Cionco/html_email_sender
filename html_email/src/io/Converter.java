c7fe512866634358f7e6e0054deb407f03c50772
Nicolas Kepper <nicolas.kepper@gmail.com>
2019-08-02 12:13:21 +0100

    Can recognize ul now and can see the end of one list so different lists dont get mixed

--- html_email/src/io/Converter.java
+++ html_email/src/io/Converter.java
@@ -13,6 +13,7 @@ public class Converter {
 		sb.append("<p>");
 		
 		ol();
+		ul();
 		WIP = WIP.replace("\n\n", "</p><p>")
 				.replaceAll("(?<!\n)\n(?!\n)", "<br>");
 		sb.append(WIP);
@@ -20,7 +21,7 @@ public class Converter {
 		return sb.toString();
 	}
 	
-	private boolean ol() {
+	private void ol() {
 		StringBuilder ol = new StringBuilder();
 		int current;
 		int start;
@@ -29,7 +30,11 @@ public class Converter {
 		
 		ol.append("<ol>");
 		
-		for(start = current = WIP.indexOf("1."), i = 1; WIP.indexOf(i + ".") >= 0 && WIP.charAt(WIP.indexOf(i + ".") - 1) == '\n'; i++, current = WIP.indexOf(i + ".")) {
+		for(start = current = WIP.indexOf("1."), i = 1; WIP.indexOf(i + ".") >= 0  //Check if next index exists
+														&& WIP.charAt(WIP.indexOf(i + ".") - 1) == '\n' //Check if next index is the start of the line
+														&& ((i > 1)?countChar(WIP.substring(WIP.indexOf(i - 1 + "."), WIP.indexOf(i + ".")), '\n') == 1:true); //Check if there is only one 
+																											//new line Character between the last index and this one if it's not the first index
+													i++, current = WIP.indexOf(i + ".")) {
 			ol.append("<li>");
 			String item = WIP.substring(current + 2, WIP.indexOf('\n', current)).trim();
 			ol.append(item);
@@ -42,6 +47,45 @@ public class Converter {
 			WIP = WIP.replace(WIP.substring(start, last_item), ol.toString());
 			ol();
 		}
-		return false;
+	}
+	
+	private void ul() {
+		StringBuilder ul = new StringBuilder();
+		int current, last_start_line = 0;
+		int start;
+		int last_item = 0;
+		int i;
+		
+		ul.append("<ul>");
+		
+		for(start = current = WIP.indexOf("-"), i = 1; current >= 0 && WIP.indexOf("-", current) >= 0  //Check if next index exists
+														&& WIP.charAt(WIP.indexOf("-", current) - 1) == '\n' //Check if next index is the start of the line
+														&& ((i > 1)?countChar(WIP.substring(WIP.indexOf("-", last_start_line), WIP.indexOf("-", current)), '\n') == 1:true); //Check if there is only one 
+																											//new line Character between the last index and this one if it's not the first index
+													i++, current = WIP.indexOf("-", (last_start_line = current) + 1)) {
+			ul.append("<li>");
+			String item = WIP.substring(current + 1, WIP.indexOf('\n', current)).trim();
+			ul.append(item);
+			ul.append("</li>");
+			last_item = WIP.indexOf('\n', current);
+		}
+		ul.append("</ul>");
+		
+		if(last_item > 0) {
+			WIP = WIP.replace(WIP.substring(start, last_item), ul.toString());
+			ul();
+		}
+	}
+	
+	private int countChar(String str, char c)
+	{
+	    int count = 0;
+
+	    for(int i=0; i < str.length(); i++)
+	    {    if(str.charAt(i) == c)
+	            count++;
+	    }
+
+	    return count;
 	}
 }
