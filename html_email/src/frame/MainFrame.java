8e602de160600aae71b898fc93979fedd2270290
Nicolas Kepper <nicolas.kepper@gmail.com>
2019-08-02 14:03:31 +0100

    Provisional Renderer added
    Can't quite get all the css yet

--- html_email/src/frame/MainFrame.java
+++ html_email/src/frame/MainFrame.java
@@ -2,15 +2,25 @@ package frame;
 
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
+import javax.swing.text.html.HTMLEditorKit;
 
 import io.Mailer;
 import lib.CenterlessBorderLayout;
@@ -21,6 +31,9 @@ public class MainFrame extends JFrame {
 	JPanel contentPane;
 	
 	String templateFile;
+	
+	JTextArea txa_source;
+	JEditorPane htmlPane;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -38,7 +51,10 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setBounds(100, 100, 450, 650);
 		
-		JTextArea txa_source = new JTextArea();
+		ChangeListener updater = new ChangeListener();
+		
+		txa_source = new JTextArea();
+		txa_source.getDocument().addDocumentListener(updater);
 		contentPane.add(txa_source, CenterlessBorderLayout.WEST);
 		
 		JPanel southPane = new JPanel();
@@ -51,6 +67,7 @@ public class MainFrame extends JFrame {
 		JButton btn_select_template = new JButton("Select Template Email");
 		btn_select_template.addActionListener(e -> {
 			JFileChooser chooser = new JFileChooser();
+			chooser.addActionListener(updater);
 			FileNameExtensionFilter filter = new FileNameExtensionFilter(
 					"HTML & Text Files", "html", "txt");
 			chooser.setFileFilter(filter);
@@ -65,6 +82,44 @@ public class MainFrame extends JFrame {
 		
 		contentPane.add(southPane, CenterlessBorderLayout.SOUTH);
 		
+		htmlPane = new JEditorPane();
+		htmlPane.setEditable(false);
+		JScrollPane htmlScroller = new JScrollPane(htmlPane);
+		HTMLEditorKit kit = new HTMLEditorKit();
+		htmlPane.setEditorKit(kit);
+		htmlPane.setDocument(kit.createDefaultDocument());
+		
+		
+		contentPane.add(htmlScroller, CenterlessBorderLayout.EAST);
+		
 		setContentPane(contentPane);
+	}
+	
+	public class ChangeListener implements DocumentListener, ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {update();}
+		@Override
+		public void insertUpdate(DocumentEvent e) {update();}
+		@Override
+		public void removeUpdate(DocumentEvent e) {update();}
+		@Override
+		public void changedUpdate(DocumentEvent e) {update();}
+
+		public void update() {
+			byte[] encoded;
+			try {
+				String html;
+				if(templateFile != null) {
+					encoded = Files.readAllBytes(Paths.get(templateFile));
+					html = new String(encoded, Charset.forName("UTF-8"));
+					html = html.replace("<div class=\"emailcontent\"></div>", "<div class=\"emailcontent\">" + new io.Converter(txa_source.getText()).convertTextToHTML() +"</div>");
+				} else
+					html = new io.Converter(txa_source.getText()).convertTextToHTML();
+				htmlPane.setText(html);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
